425baf556e15039973244094848f0059922e7648
BryceDay-GradPC <brycecday23@gmail.com>
2025-10-28 17:20:29 -0500

    Finished adding CSV, time, and logging. Removed pickle. Should be ready to run.

--- KukaInnerBoundCheck.py
+++ KukaInnerBoundCheck.py
@@ -2,12 +2,9 @@ import roboticstoolbox as rtb
 import numpy as np
 import spatialmath as sm
 import math
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 import logging
 import csv
 import time
-import pickle
 
 
 def fibonacci_sphere(samples=1000):
@@ -50,10 +47,13 @@ def time_difference(first_time, second_time, need_hours):
     return start_to_end_difference_formatted
 
 
-logging.basicConfig(filename="KukaBoundCheck.log", level=logging.INFO)
+logging.basicConfig(filename="KukaBoundCheckLog.log", level=logging.INFO)
 
-orientation_samples = 180*180*2
-bound_samples = 180
+fields = ['position', 'approach', 'orientation',
+          'alt orientation', 'solution boolean', 'solution', 'total possible orientations']
+
+orientation_samples = 90*90*2
+bound_samples = 180*2
 bound_location = 284.52767879  # mm
 
 angle_step = math.pi/bound_samples
@@ -82,10 +82,6 @@ for point in approach_sphere:
     if point[1] >= 0:
         approach_vectors.append(point)
 
-with open('testing.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows([['Approach vectors'], [pickle.dumps([[approach_vectors]])]])
-
 orientation_vectors = []
 for point_num in range(len(approach_vectors)):
     attempt = 1
@@ -102,51 +98,77 @@ alt_orientation_vectors = []
 for point in orientation_vectors:
     alt_orientation_vectors.append(np.array(np.array(point)*-1).tolist())
 
-x_points = []
+'''x_points = []
 z_points = []
 for location in workspace_locations:
     x_points.append(location[0])
     z_points.append(location[2])
 plt.gca().set_aspect('equal')
 plt.plot(x_points, z_points, 'o')
-plt.show()
+plt.show()'''
 
 lbr = rtb.models.URDF.LBR()                  # instantiate robot model
 print(lbr)
 
-possible_orientations = []
-location_count = 0
-for location in workspace_locations:
-    location_count += 1
-    orientation_count = 0
-    for vector_num in range(len(approach_vectors)):
-        Tep = (sm.SE3.Trans(location[0], location[1], location[2])
-               * sm.SE3.OA([orientation_vectors[vector_num][0],
-                           orientation_vectors[vector_num][1],
-                           orientation_vectors[vector_num][2]],
-                           [approach_vectors[vector_num][0],
-                           approach_vectors[vector_num][1],
-                           approach_vectors[vector_num][2]]))
-        sol = lbr.ik_LM(Tep, joint_limits=True)
-        if sol[1] == 1:
-            orientation_count += 1
-        else:
+with open('testing.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+
+    possible_orientations = []
+    location_count = 0
+    start_time = int(time.time()*1000)
+    for location in workspace_locations:
+        location_count += 1
+        orientation_count = 0
+        location_time_start = int(time.time()*1000)
+        for vector_num in range(len(approach_vectors)):
             Tep = (sm.SE3.Trans(location[0], location[1], location[2])
-                   * sm.SE3.OA([alt_orientation_vectors[vector_num][0],
-                               alt_orientation_vectors[vector_num][1],
-                               alt_orientation_vectors[vector_num][2]],
+                   * sm.SE3.OA([orientation_vectors[vector_num][0],
+                               orientation_vectors[vector_num][1],
+                               orientation_vectors[vector_num][2]],
                                [approach_vectors[vector_num][0],
                                approach_vectors[vector_num][1],
                                approach_vectors[vector_num][2]]))
             sol = lbr.ik_LM(Tep, joint_limits=True)
             if sol[1] == 1:
                 orientation_count += 1
-        logging.info("Location: " + str(location_count) + "/" + str(len(workspace_locations)) +
-                     ", Orientation: " + str(vector_num) + "/" + str(len(approach_vectors)) +
-                     ", Possible Orientations Found: " + str(possible_orientations))
-        print("Location: " + str(location_count) + "/" + str(len(workspace_locations)) +
-              ", Orientation: " + str(vector_num) + "/" + str(len(approach_vectors)) +
-              ", Possible Orientations Found: " + str(possible_orientations))
-    possible_orientations.append(orientation_count)
+            else:
+                Tep = (sm.SE3.Trans(location[0], location[1], location[2])
+                       * sm.SE3.OA([alt_orientation_vectors[vector_num][0],
+                                   alt_orientation_vectors[vector_num][1],
+                                   alt_orientation_vectors[vector_num][2]],
+                                   [approach_vectors[vector_num][0],
+                                   approach_vectors[vector_num][1],
+                                   approach_vectors[vector_num][2]]))
+                sol = lbr.ik_LM(Tep, joint_limits=True)
+                print(sol)
+                if sol[1] == 1:
+                    orientation_count += 1
+                now_time = int(time.time()*1000)
+                location_running = time_difference(location_time_start, now_time, False)
+                total_running = time_difference(start_time, now_time, True)
+            logging.info("Location: " + str(location_count) + "/" + str(len(workspace_locations)) +
+                         "\nOrientation: " + str(vector_num) + "/" + str(len(approach_vectors)) +
+                         "\nPossible Orientations Found: " + str(orientation_count) +
+                         "\nTime at this location: " + str(location_running) +
+                         "\nTotal time running: " + str(total_running) +
+                         "\n"
+                         )
+
+            '''print("Location: " + str(location_count) + "/" + str(len(workspace_locations)) +
+                  ", Orientation: " + str(vector_num) + "/" + str(len(approach_vectors)) +
+                  ", Possible Orientations Found: " + str(possible_orientations))'''
+
+            if vector_num is len(approach_vectors)-1:
+                csvwriter.writerows([[location, approach_vectors[vector_num], tuple(orientation_vectors[vector_num]),
+                                      tuple(alt_orientation_vectors[vector_num]),
+                                      sol[1], tuple(sol[0]), orientation_count]])
+            else:
+                csvwriter.writerows([[location, approach_vectors[vector_num], tuple(orientation_vectors[vector_num]),
+                                      tuple(alt_orientation_vectors[vector_num]),
+                                      sol[1], tuple(sol[0])]])
+        logging.info('##########################################################################################')
+        possible_orientations.append(orientation_count)
+
 
 print('Finished :' + str(possible_orientations))
