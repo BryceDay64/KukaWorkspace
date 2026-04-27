9c61027833eaaba500832f79b668c648c597b61f
BryceDay64 <brycecday23@gmail.com>
2026-04-27 15:53:24 -0500

    Added tqdm and labels to figure

--- WorkspaceFigureCreator.py
+++ WorkspaceFigureCreator.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import csv
+from tqdm import tqdm
 
 # TODO: Import CSV
 #       count successful orientations at each location
@@ -15,10 +16,7 @@ with open('iiwa141762967540628.9177.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile)
     csvreader = list(csvreader)
     current_location = [csvreader[1][0][0], csvreader[1][0][2]]
-    for row in range(1, len(csvreader)):
-        print(str(row) + " / " + str(len(csvreader)))
-        print(current_location)
-        print(orientation_count)
+    for row in tqdm(range(1, len(csvreader))):
         x = tuple(map(float, csvreader[row][0].replace("(", "").replace(")", "").split(', ')))[0]
         z = tuple(map(float, csvreader[row][0].replace("(", "").replace(")", "").split(', ')))[2]
         if x == current_location[0] and z == current_location[1]:
@@ -33,8 +31,13 @@ with open('iiwa141762967540628.9177.csv', newline='') as csvfile:
     locations_z.append(z)
     orientations.append(orientation_count)
 
+print(max(orientations))
 
 [float(i) for i in orientations]
-plt.scatter(locations_x, locations_z, s=1**2, c=orientations, cmap='tab20b', vmin=0, vmax=500)  # 'tab20b' 'YlOrRd' 'tab20c'
-plt.colorbar()
+fig, ax = plt.subplots()
+im = ax.scatter(locations_x, locations_z, s=1**2, c=orientations, cmap='tab20b', vmin=0, vmax=500)  # 'tab20b' 'YlOrRd' 'tab20c'
+cbar = fig.colorbar(im, ax=ax)
+plt.xlabel('x (m)')
+plt.ylabel('z (m)')
+cbar.set_label("Number of valid approaches")
 plt.show()
